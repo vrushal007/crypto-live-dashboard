e750d7d0b6ef1301b429bef4f7a4a65a3717f605
vrushal007 <vrushalkpatel007@gmail.com>
2023-07-05 23:05:18 +0530

    err commented

--- src/components/CryptoDetailItem.js
+++ src/components/CryptoDetailItem.js
@@ -9,30 +9,30 @@ function CryptoDetailItem () {
   const [itemData, setItemData] = useState({})
   const [updatedPrice, setUpdatedPrice] = useState()
   const [loading, setLoading] = useState(false)
-  const [isError,setIsError] = useState()
+  const [isError, setIsError] = useState(false)
   const socket = new WebSocket(`wss://ws.coincap.io/prices?assets=${params.id}`)
 
   useEffect(() => {
-    const fetchItem = async () => {
+    (async () => {
       setLoading(true)
+      setIsError(false)
       const response = await fetch(
         `https://api.coincap.io/v2/assets/${params.id}`
       )
-      if(!response.ok){
+      if (!response.ok) {
         setLoading(false)
-        setIsError(response.status)
-        return;
+        setIsError(true)
       }
-      setLoading(false)
-      const resData = await response.json()
-      setItemData(resData.data)
-    }
-    fetchItem()
+      else{
+        setLoading(false)
+        const resData = await response.json()
+        setItemData(resData.data)
+      }
+    })()
   }, [params])
 
   socket.onmessage = message => {
     const data = message.data
-    // console.log(JSON.parse(data))
     setUpdatedPrice(JSON.parse(data))
   }
   socket.onerror = e => {
@@ -43,68 +43,71 @@ function CryptoDetailItem () {
   }
   return (
     <div>
-      {isError=== 404 ? <h1>404! The page you are trying to access doesn't exist</h1> :
-    <div className={classes.main}>
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <div className={classes.container}>
-          <h1>{itemData.name}</h1>
-          <div className={classes.item}>
-            <p>Symbol:</p>
-            <p>{itemData.symbol}</p>
-          </div>
-          <hr />
-          <div className={classes.item}>
-            <p>Price (In USD):</p>
-            <p>{updatedPrice ? updatedPrice[params.id] : itemData.priceUsd}</p>
-          </div>
-          <hr />
-          <div className={classes.item}>
-            <p>Change % (Last 24 hr):</p>
-            <p
-              className={
-                itemData.changePercent24Hr < 0
-                  ? classes.negative
-                  : classes.positive
-              }
-            >
-              {itemData.changePercent24Hr}%
-            </p>
-          </div>
-          <hr />
-          <div className={classes.item}>
-            <p>Market Cap (In USD):</p>
-            <p>{itemData.marketCapUsd}</p>
-          </div>
-          <hr />
-          <div className={classes.item}>
-            <p>Volume used (last 24 hr):</p>
-            <p>{itemData.volumeUsd24Hr}</p>
-          </div>
-          <hr />
-          <div className={classes.item}>
-            <p>Supply:</p>
-            <p>{itemData.supply}</p>
-          </div>
-          <hr />
-          <div className={classes.item}>
-            <p>Max Supply:</p>
-            <p>{itemData.maxSupply ? itemData.maxSupply : 'NA'}</p>
-          </div>
+      {!loading && isError && <h1>Something wrong</h1>}
+      {loading && !isError && <CircularProgress />}
+      {!isError && (
+        <div className={classes.main}>
+          {!loading && !isError && (
+            <div className={classes.container}>
+              <h1>{itemData.name}</h1>
+              <div className={classes.item}>
+                <p>Symbol:</p>
+                <p data-testid='symbol'>{itemData.symbol}</p>
+              </div>
+              <hr />
+              <div className={classes.item}>
+                <p>Price (In USD):</p>
+                <p data-testid='price'>
+                  {updatedPrice ? updatedPrice[params.id] : itemData.priceUsd}
+                </p>
+              </div>
+              <hr />
+              <div className={classes.item}>
+                <p>Change % (Last 24 hr):</p>
+                <p
+                  className={
+                    itemData.changePercent24Hr < 0
+                      ? classes.negative
+                      : classes.positive
+                  }
+                >
+                  {itemData.changePercent24Hr}%
+                </p>
+              </div>
+              <hr />
+              <div className={classes.item}>
+                <p>Market Cap (In USD):</p>
+                <p>{itemData.marketCapUsd}</p>
+              </div>
+              <hr />
+              <div className={classes.item}>
+                <p>Volume used (last 24 hr):</p>
+                <p>{itemData.volumeUsd24Hr}</p>
+              </div>
+              <hr />
+              <div className={classes.item}>
+                <p>Supply:</p>
+                <p>{itemData.supply}</p>
+              </div>
+              <hr />
+              <div className={classes.item}>
+                <p>Max Supply:</p>
+                <p>{itemData.maxSupply ? itemData.maxSupply : 'NA'}</p>
+              </div>
+            </div>
+          )}
+          <button
+            onClick={() => {
+              navigate('/')
+            }}
+            className={classes.btn}
+            data-testid='backBtn'
+          >
+            Back to home
+          </button>
         </div>
       )}
-      <button
-        onClick={() => {
-          navigate('/')
-        }}
-        className={classes.btn}
-      >
-        Back to home
-      </button>
-    </div>}
     </div>
-    
   )
 }
 
